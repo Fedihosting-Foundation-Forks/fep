b5a1ddf8c8c33e5dec860de75d57b9f23da6614f
bumblefudge <bumblefudge@learningproof.xyz>
2024-07-05 20:52:19 +0200

    remove testing artefacts from local copy for cleaner initial FEP

--- fep/0f2a/activitypub-testing-fep-0f2a/fep/0f2a/index.js
+++
@@ -1,7 +0,0 @@
-import fep0f2aTest from "./fep-0f2a-actor-object-tombstone-syntax.js"
-
-export default {
-  tests: [
-    fep0f2aTest,
-  ],
-}
--- fep/0f2a/activitypub-testing-fep-0f2a/index.js
+++
@@ -1,3 +0,0 @@
-import _fep0f2a from "./fep/0f2a/index.js"
-
-export const fep0f2a = _fep0f2a
