f3749ec6bd2daa9ec1ec0f66b84c8ec0c3d71534
silverpill <silverpill@firemail.cc>
2024-08-28 11:40:36 +0000

    Fix tests

--- scripts/tools/test_index.py
+++ scripts/tools/test_index.py
@@ -1,13 +1,13 @@
 import pytest
 
-from scripts.tools import Readme
+from scripts.tools import FepIndex
 
 
 @pytest.mark.skip("Only is correct for main branch and not pull requests")
-def test_readme():
-    with open("README.md") as f:
+def test_index():
+    with open("index.md") as f:
         lines = f.readlines()
 
-    expected = Readme().content
+    expected = FepIndex().content
 
     assert lines == expected
